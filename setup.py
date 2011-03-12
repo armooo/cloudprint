b5bede80586e9dd9f9bc536620b754add4ac63a0
Jason Michalski <armooo@armooo.net>
2011-03-12 12:25:48 -0500

    Bump version 0.3

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ import os.path
 
 setup(
     name='cloudprint',
-    version='0.2',
+    version='0.3',
     description='Google cloud print proxy for linux/OSX',
     long_description=open('README.rst').read(),
     author='Jason Michalski',
