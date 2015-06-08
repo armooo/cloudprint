a2fdab3f44ff25937c7591d95e125add782bcf4f
Jason Michalski <armooo@armooo.net>
2015-06-07 21:57:31 -0700

    Add dependencies and bump version

--- setup.py
+++ setup.py
@@ -5,17 +5,15 @@ except ImportError:
     use_setuptools()
     from setuptools import setup, find_packages
 
-import os.path
-
 setup(
     name='cloudprint',
-    version='0.11',
+    version='0.12',
     description='Google cloud print proxy for linux/OSX',
     long_description=open('README.rst').read(),
     author='Jason Michalski',
     author_email='armooo@armooo.net',
     url='https://github.com/armooo/cloudprint',
-    classifiers = [
+    classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Console',
         'Intended Audience :: End Users/Desktop',
@@ -25,9 +23,12 @@ setup(
         'License :: OSI Approved :: GNU General Public License (GPL)',
     ],
     packages=find_packages(exclude=['ez_setup']),
-    entry_points = {
+    entry_points={
         'console_scripts': [
             'cloudprint = cloudprint.cloudprint:main',
         ],
     },
+    install_requires=[
+        'requests==2.7.0',
+    ],
 )
